f9f45d052e8f7eea2373cebfcf07e4ff8b63e18d
Albert Weinert <info@der-albert.com>
2012-10-16 00:07:27 +0200

    removed vs upgrade reports and backups

--- src/Backup/UserGroup.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UserGroup.Web.MvcApplication" Language="C#" %>
--- src/Backup/UserGroup.Web/Models/ShowHomeIndexModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserGroup.Web.Models
-{
-    public class ShowHomeIndexModel
-    {
-        public MeetingLineModel NextMeeting { get; set; }
-        public ShowPageModel Page { get; set; }
-    }
-}
