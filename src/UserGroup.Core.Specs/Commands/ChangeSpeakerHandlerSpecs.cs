0d0b6cfe0625395512b7a71b28f68cb1df1c3d25
Albert Weinert <info@der-albert.com>
2013-08-18 19:43:43 +0200

    implemented ChangeSpeaker-Command

---
+++ src/UserGroup.Core.Specs/Commands/ChangeSpeakerHandlerSpecs.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Aperea.Data;
+using Machine.Fakes.Sdk;
+using Machine.Specifications;
+using Machine.Fakes;
+using UserGroup.Commands;
+using UserGroup.Commands.Handlers;
+using UserGroup.Entities;
+
+namespace UserGroup.Core.Specs.Commands
+{
+    [Subject(typeof (ChangeSpeakerHandler))]
+    public class When_changing_the_speaker_data : WithSubject<ChangeSpeakerHandler>
+    {
+        Establish context = () => {  };
+
+        Because of = () =>
+        {
+            speakers = TestData.GetFiveSpeakers();
+            With(new BehaviorRepository<Speaker>(speakers));
+            With<BehaviorCoreMapping>();
+
+            var command = new ChangeSpeaker
+            {
+                Id = speakers[2].Id,
+                GivenName = "Hover",
+                LastName = "Craft",
+                Slug = "foo-bar"
+            };
+            Subject.Execute(command);
+            speaker = The<IRepository<Speaker>>().Entities.Single(s => s.Id == command.Id);
+        };
+
+        It should_save_the_changes = () => The<IRepository<Speaker>>().WasToldTo(r=>r.SaveAllChanges());
+        It should_have_hover_as_given_name=()=>speaker.GivenName.ShouldEqual("Hover");
+        It should_have_craft_as_last_name=()=>speaker.LastName.ShouldEqual("Craft");
+        It should_have_foo_bar_as_slug = ()=>speaker.Slug.ShouldEqual("foo-bar");
+        static Speaker[] speakers;
+        static Speaker speaker;
+    }
+}
---
+++ src/UserGroup.Core/Commands/Handlers/ChangeSpeakerHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Aperea.Commands;
+using Aperea.Data;
+using Aperea.Infrastructure.Mappings;
+using UserGroup.Entities;
+
+namespace UserGroup.Commands.Handlers
+{
+    public class ChangeSpeakerHandler : CommandHandler<ChangeSpeaker>
+    {
+        private readonly IRepository<Speaker> repository;
+        public ChangeSpeakerHandler(IRepository<Speaker> repository)
+        {
+            this.repository = repository;
+        }
+
+        public override void Execute(ChangeSpeaker command)
+        {
+            var speaker = repository.Entities.Single(s => s.Id == command.Id);
+            command.MapTo(speaker);
+            repository.SaveAllChanges();
+        }
+    }
+}
